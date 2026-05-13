0a0bbe0f4d5660f9bf7249d4414f84575575a417
bradar <bradar@whois.dis>
2026-05-13 19:53:35 +0800

    fix: update run script to use port 443 with startup delays

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,10 +2,12 @@
 
 echo "Starting ttyd on port 8080..."
 ttyd -p 8080 bash &
+sleep 1
 
 echo "Starting FileManager on port 8081 without authentication..."
 filebrowser -p 8081 -r /home/runner/work/ --noauth &
+sleep 5 #waiting for services to properly start
 
 echo "---------------------------------------------------"
 echo "CLICK THIS LINK BELOW TO ACCESS YOUR WEB :"
-ssh -o StrictHostKeyChecking=no -R 80:localhost:8080 a.pinggy.io
+ssh -o StrictHostKeyChecking=no -p 443 -R 80:localhost:8080 a.pinggy.io
